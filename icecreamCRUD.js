65ade078b7ce2b4483d41aa4411f2773d542c504
soheila <soheilalotfi@ymail.com>
2019-08-12 12:53:12 -0400

    install mysql package-connect js file to database using createConnection function

---
+++ icecreamCRUD.js
@@ -0,0 +1,9 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Leila@1357",
+  database: icecream_db
+});
