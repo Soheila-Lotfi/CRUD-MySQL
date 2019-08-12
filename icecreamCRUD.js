25f0c84b39ada7f33e4080a78d7c56aa300f69a3
soheila <soheilalotfi@ymail.com>
2019-08-12 13:17:12 -0400

    add creatProduct function

--- icecreamCRUD.js
+++ icecreamCRUD.js
@@ -5,5 +5,26 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "Leila@1357",
-  database: icecream_db
+  database: "icecream_db"
 });
+
+connection.connect(function(err) {
+  if (err) throw err;
+  CreateProduct();
+});
+
+// insert data to iceCream table ----------------- CREATE---------------------------
+function CreateProduct() {
+  connection.query(
+    "INSERT INTO iceCream SET ?",
+    {
+      flavor: "strawberry",
+      price: 3.14,
+      quantity: 5
+    },
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + "");
+    }
+  );
+}
